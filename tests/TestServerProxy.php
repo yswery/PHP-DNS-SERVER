19e70bd0c4a8cc318c7991544d6d56a23647b0e8
samuelwilliams <samuel.williams8@gmail.com>
2017-04-24 17:13:57 +1000

    Added tests for the server class.

---
+++ tests/TestServerProxy.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * This file is part of PHP DNS Server.
+ *
+ * (c) Yif Swery <yiftachswr@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace yswery\DNS\Tests;
+
+use yswery\DNS\Server;
+use yswery\DNS\JsonStorageProvider;
+
+class TestServerProxy
+{
+    private static $name = 'yswery\DNS\Server';
+
+    /**
+     * @var Server
+     */
+    protected $server;
+
+    public function __construct()
+    {
+        $storage = new JsonStorageProvider(__DIR__ . '/test_records.json');
+        $this->server = new Server($storage);
+    }
+
+    public function ds_encode_label($str, $offset = null)
+    {
+        $method = new \ReflectionMethod(self::$name, 'ds_encode_label');
+        $method->setAccessible(true);
+
+        return $method->invoke($this->server, $str, $offset);
+    }
+}
