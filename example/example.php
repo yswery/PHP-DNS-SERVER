2d86298cc80e20e0ba1d7e428dd082eb82ac9d01
Ivan Stanojevic <ivan@byteout.com>
2018-04-14 19:24:49 +0200

    changed paths in example.php

--- example/example.php
+++ example/example.php
@@ -11,13 +11,13 @@ use yswery\DNS\Server;
 // instantiate resolver
 $resolver = new StackableResolver(
     [
-        new JsonResolver('config/dns.example.json'), // JSON formatted DNS records file
+        new JsonResolver('../config/dns.example.json'), // JSON formatted DNS records file
         new RecursiveResolver(),                         // Recursive provider acting as a fallback to the JsonStorageProvider
     ]
 );
 
 // load configuration
-$config = Yaml::parseFile('config/config.yml');
+$config = Yaml::parseFile('../config/config.yml');
 
 // Creating a new instance of server and start it
 (new Server($resolver, $config))->run();
