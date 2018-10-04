f766d1e0c58d639bf326645496a4d8dcee43e7ff
Samuel Williams <samuel.williams8@gmail.com>
2018-10-04 14:13:46 +1000

    Added test for StackableResolver.

---
+++ src/Tests/Resolver/StackableResolverTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace yswery\DNS\Tests\Resolver;
+
+use yswery\DNS\Resolver\JsonResolver;
+use yswery\DNS\Resolver\StackableResolver;
+use yswery\DNS\Resolver\XmlResolver;
+use yswery\DNS\Resolver\YamlResolver;
+
+class StackableResolverTest extends JsonResolverTest
+{
+    /**
+     * @throws \Exception
+     */
+    public function setUp()
+    {
+        $jsonFiles = [
+            __DIR__.'/../Resources/example.com.json',
+            __DIR__.'/../Resources/test_records.json',
+        ];
+
+        $xmlFiles = [
+            __DIR__.'/../Resources/example.com.xml',
+            __DIR__.'/../Resources/test.com.xml',
+            __DIR__.'/../Resources/test2.com.xml',
+        ];
+
+        $ymlFiles = [
+            __DIR__.'/../Resources/records.yml',
+            __DIR__.'/../Resources/example.com.yml',
+        ];
+
+        $this->resolver = new StackableResolver([
+            new JsonResolver($jsonFiles),
+            new XmlResolver($xmlFiles),
+            new YamlResolver($ymlFiles),
+        ]);
+
+    }
+}
